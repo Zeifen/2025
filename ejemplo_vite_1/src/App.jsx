d679c0cd4a92cf2f0ea33c15bf8155e002831db2
Zeifen <60271815+Zeifen@users.noreply.github.com>
2025-02-18 15:46:47 -0600

    Primer commit para nuevo repo

---
+++ ejemplo_vite_1/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="card">
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
